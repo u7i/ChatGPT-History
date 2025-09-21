8a3cb24952837ef2e25a884798ce98677a780945
u7i <umnicodeusa@gmail.com>
2025-09-21 10:32:55 +0300

    Feat: Skeleton of the plugin

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const common = {
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+        minimize: false
+    },
+    target: ['web']
+}
+
+module.exports = [
+    {
+        entry: './src/dummy.js',
+        output: {
+            filename: 'ignore.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [{ from: 'public' }]
+            })
+        ],
+    },
+
+    // Content Script for ChatGPT
+    {
+        entry: './src/front/index.ts',
+        output: {
+            filename: 'content-chatgpt.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        ...common
+    },
+
+    // Background script
+    {
+        entry: './src/worker/index.ts',
+        output: {
+            filename: 'background-worker.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        ...common
+    }
+];
